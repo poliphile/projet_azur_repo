683f60555329e6f5de9b5adcc5b0bd882b92566f
sekioa <simon.fuger@gmail.com>
2015-06-11 15:09:25 +0200

    uydu

---
+++ RestWebService/src/mywebapp/java/main/services/UtilisateurSerieService.java
@@ -0,0 +1,92 @@
+/**
+ *
+ */
+package mywebapp.java.main.services;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import mywebapp.java.main.persistance.daoimpl.UtilisateurSerieDAOImpl;
+import mywebapp.java.main.persistance.object.UtilisateurSerieDO;
+import mywebapp.java.main.presentation.serie.bean.UtilisateurSerieDTO;
+import mywebapp.java.main.presentation.utilisateur.bean.UtilisateurDTO;
+
+/**
+ * @author Sekioa
+ *
+ */
+public class UtilisateurSerieService {
+
+	private static Map<UtilisateurDTO, Map<String, String>> listeUtilisateurDTO;
+
+	private static UtilisateurSerieService instance;
+
+	private final UtilisateurSerieDAOImpl utilisateurSerieDAO = new UtilisateurSerieDAOImpl();
+
+	private UtilisateurSerieService() {
+		// empty method
+	}
+
+	public static UtilisateurSerieService getInstance() {
+		if (instance == null) {
+			return new UtilisateurSerieService();
+		} else {
+			return instance;
+		}
+	}
+
+	public static Map<UtilisateurDTO, Map<String, String>> getListeUtilisateurDTO() {
+		if (listeUtilisateurDTO == null) {
+			listeUtilisateurDTO = new HashMap<UtilisateurDTO, Map<String, String>>();
+			return listeUtilisateurDTO;
+		} else {
+			return listeUtilisateurDTO;
+		}
+	}
+
+	public boolean creerUtilisateurSerie(
+			final UtilisateurSerieDTO utilisateurSerieDTO,
+			final UtilisateurDTO utilisateurDTO) {
+
+		final UtilisateurSerieDO utilisateurSerieDO = convertUtilisateurSerieDTOtoDO(utilisateurSerieDTO);
+
+		final boolean result = utilisateurSerieDAO
+				.creerUtilisateurSerieDO(utilisateurSerieDO);
+		return result;
+	}
+
+	public boolean modifierUtilisateurSerie(
+			final UtilisateurSerieDTO utilisateurSerieDTO) {
+
+		final UtilisateurSerieDO utilisateurSerieDO = convertUtilisateurSerieDTOtoDO(utilisateurSerieDTO);
+
+		final boolean result = utilisateurSerieDAO
+				.modifierUtilisateurSerieDO(utilisateurSerieDO);
+
+		return result;
+	}
+
+	public boolean supprimerUtilisateurSerie(
+			final UtilisateurSerieDTO utilisateurSerieDTO) {
+
+		final UtilisateurSerieDO utilisateurSerieDO = convertUtilisateurSerieDTOtoDO(utilisateurSerieDTO);
+
+		final boolean result = utilisateurSerieDAO
+				.supprimerUtilisateurSerieDO(utilisateurSerieDO);
+
+		return result;
+	}
+
+	private UtilisateurSerieDO convertUtilisateurSerieDTOtoDO(
+			final UtilisateurSerieDTO utilisateurSerieDTO) {
+
+		final UtilisateurSerieDO utilisateurSerieDO = new UtilisateurSerieDO();
+
+		utilisateurSerieDO.setId_serie(utilisateurSerieDTO.getId_serie());
+		utilisateurSerieDO.setId_utilisateur(utilisateurSerieDTO
+				.getId_utilisateur());
+
+		return utilisateurSerieDO;
+	}
+
+}
